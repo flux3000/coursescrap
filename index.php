5225088f36f72fe99d23ec2777580b9063f7b854
tomnar <tomquast89@gmail.com>
2013-10-22 23:07:56 -0700

    added content to popbox

--- index.php
+++ index.php
@@ -36,18 +36,22 @@
 	<div class="popup">
 		<div class="close">x</div>
 		<h2>How this prototype relates to metacrap</h2>
-		<p>There are several points of meta-crap this prototype touches, in general these points a worth mentioning:</p>
+		<p>We touch most of the points made by <a href="http://www.well.com/~doctorow/metacrap.htm">Doctorow</a>. Generally we think his points are valid, but we as site-owners tried to minimize their impact.</a></p>
 		<dl>
-			<dt>One point</dt>
-			<dd>something</dd>
-			<dt>another point</dt>
-			<dd>something</dd>
-			<dt>another point</dt>
-			<dd>something</dd>
-			<dt>another point</dt>
-			<dd>something</dd>
-			<dt>another point</dt>
-			<dd>something</dd>
+			<dt>People lie</dt>
+			<dd>Tags can be voted up and/or down. We rely on the collective intelligence to make lying hard.</dd>
+			<dt>People are lazy</dt>
+			<dd>All input fields use auto-completion. Thereby people don't need to spell things out in order to enter them.</dd>
+			<dt>People are stupid</dt>
+			<dd>By auto-completing people get hints what actions are possible. They are guided through the process.</dd>
+			<dt>Mission: Impossible -- know thyself</dt>
+			<dd>Since tags are collected collectively one single person does not need to know all relevant tags.</dd>
+			<dt>Schemas aren't neutral</dt>
+			<dd>We use the same tags for all courses. They might not be neutral itself but all courses have the same tags available.</dd>
+			<dt>Metrics influence results</dt>
+			<dd>We present a relative homogeneous group, the controlled vocabulary should be fitting for all courses.</dd>
+			<dt>There are many ways to describe something</dt>
+			<dd>Users can only use a finite number of tags. Thereby they will hopefully over time generate a consensus about the best fitting tags.</dd>
 		</dl>
 	</div>
 </body>
